027f470b76ddd2e99e56e4585d8b5c53b37768fb
KelvinYin <yinkangxi@gmail.com>
2017-04-25 23:14:11 +0800

    modify output dir name

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -x
 
 SOURCE_DIR=`pwd`
-BUILD_DIR=${BUILD_DIR:-../build/simplecodec}
+BUILD_DIR=${BUILD_DIR:-../build/binarycodec}
 BUILD_TYPE=${BUILD_TYPE:-release}
 INSTALL_DIR=${INSTALL_DIR:-../${BUILD_TYPE}-install}
 BUILD_NO_EXAMPLES=${BUILD_NO_EXAMPLES:-0}
